9389bddc9da36ab33f33b565c4f38996c0afae3a
unknown <malakalkindi1995@gmail.com>
2023-05-21 09:40:58 +0400

    employee service

--- src/main/java/com/example/demoSalaryApp/controllers/employeeController.java
+++ src/main/java/com/example/demoSalaryApp/controllers/employeeController.java
@@ -3,6 +3,18 @@ package com.example.demoSalaryApp.controllers;
 import com.example.demoSalaryApp.models.BaseEntity;
 
 
+import com.example.demoSalaryApp.models.accountModel;
+import com.example.demoSalaryApp.services.accountService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demoSalaryApp.responseopject.GetAccountResponse;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
 public class employeeController extends BaseEntity {
 
 }
--- src/main/java/com/example/demoSalaryApp/services/employeeService.java
+++ src/main/java/com/example/demoSalaryApp/services/employeeService.java
@@ -1,4 +1,62 @@
 package com.example.demoSalaryApp.services;
 
+
+import com.example.demoSalaryApp.Repository.employeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demoSalaryApp.models.employeeModel;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+//import com.example.demoSalaryApp.responseopject.GetAccountResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class employeeService {
+
+    @Autowired
+    employeeRepository employeeReps;
+
+    public void saveAccount(employeeModel emp) {
+        employeeReps.save(emp);
+    }
+
+    public List<employeeModel> getAccount() {
+        return employeeReps.findAll();
+    }
+
+    public List<employeeModel> getOneAccount() {
+        return employeeReps.findAllById(Collections.singleton((long) 2));
+    }
+
+
+//    public GetAccountResponse getEmployeeById(Long employeeId) {
+//        Optional<accountModel> optionalaccount = accountresp.findById(employeeId);
+//        if (!optionalaccount.isEmpty()) {
+//            accountModel account = optionalaccount.get();
+//            GetAccountResponse employeeResponse = new GetAccountResponse(account.getBankName(), account.getAccountNumber(),
+//                    account.getBankBranch(), account.getSwiftCode());
+//            return employeeResponse;
+//        }
+//
+//        return null;
+//
+//    }
+
+//    public GetAccountResponse getAccountObjectAsString(accountModel account) {
+//
+//
+//        GetAccountResponse employeeResponse = new GetAccountResponse(account.getBankName(), account.getAccountNumber(),
+//                account.getBankBranch(), account.getSwiftCode());
+//        return employeeResponse;
+//
+//    }
+
+//    public void deleteEmployeeById(Long employeeId) {
+//
+//        accountresp.deleteById(employeeId);
+//
+//    }
 }
